481c67252fc0b8bdd7f979bd081ce73f47979a28
= <adniyayousaf92@gmail.com>
2024-03-07 16:07:54 +0000

    completed level 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import HighScoreTable from "./HighScoreTable";
 import allCountryScores from "./data/scores.js";
 
-
 const App = () => (
   <div className="container">
     <h1 className="heading">High Scores</h1>
@@ -10,7 +9,6 @@ const App = () => (
     ))}
   </div>
 );
-
 
 //Sort the country name Alphabetically
 allCountryScores.sort(function (a, b) {
@@ -23,6 +21,17 @@ allCountryScores.sort(function (a, b) {
   return 0;
 });
 
-
+//Sort the scores in descending
+allCountryScores.forEach((item) => {
+  item.scores.sort(function (a, b) {
+    if (a.s < b.s) {
+      return 1;
+    }
+    if (a.s > b.s) {
+      return -1;
+    }
+    return 0;
+  });
+});
 
 export default App;
