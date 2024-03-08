7882c923e27eb4fb7d14c72eee25f4d58bf93602
= <adniyayousaf92@gmail.com>
2024-03-08 17:38:00 +0000

    added styles

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,18 @@ const App = () => {
         item.scores.sort((a, b) => b.s - a.s);
       }
     });
-
+    // Toggle order
     setOrder(order === "Asc" ? "Desc" : "Asc");
   }
-
-  //Sort the country name Alphabetically
-  allCountryScores.sort((a, b) => a.name - b.name);
+  allCountryScores.sort(function (a, b) {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
 
   return (
     <div className="container">
